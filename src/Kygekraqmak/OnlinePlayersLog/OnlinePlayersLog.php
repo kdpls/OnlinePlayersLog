0e013f3de173c191f5829ad0f3fecd484f16a352
KygekDev <68368066+KygekDev@users.noreply.github.com>
2021-10-03 11:21:46 +0800

    Upload plugin code

---
+++ src/Kygekraqmak/OnlinePlayersLog/OnlinePlayersLog.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kygekraqmak\OnlinePlayersLog;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\TaskScheduler;
+use pocketmine\event\Listener;
+use pocketmine\scheduler\Task;
+
+use Kygekraqmak\OnlinePlayersLog\Tasks;
+
+class OnlinePlayersLog extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getScheduler()->scheduleRepeatingTask(new Tasks($this), 5 * 60 * 20);
+    }
+
+}
---
+++ src/Kygekraqmak/OnlinePlayersLog/Tasks.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kygekraqmak\OnlinePlayersLog;
+
+use pocketmine\scheduler\Task;
+
+use Kygekraqmak\OnlinePlayersLog\OnlinePlayersLog;
+
+class Tasks extends Task {
+
+    private $main;
+
+    public function __construct(OnlinePlayersLog $main) {
+        $this->main = $main;
+    }
+
+    public function main() {
+        return $this->main;
+    }
+
+    public function onRun(int $ticks) {
+        $server = $this->main()->getServer();
+        $players = $server->getOnlinePlayers();
+        $plynames = '';
+        if (count($players) > 0) {
+            foreach ($players as $player) {
+                $plynames .= $player->getName() . ' (' . $this->getPlayerLocation($player->getAddress()) . ')' . ', ';
+            }
+        }
+        $logs = fopen($this->main()->getDataFolder().'onlineplayerslog.log', 'a+');
+        $write = '[OnlinePlayersLog] '.date('[d-m-Y H:i:s]').' There were '.count($players).' players online: '.$plynames.PHP_EOL;
+        fwrite($logs, $write);
+        fclose($logs);
+    }
+	
+	public function getPlayerLocation(string $ip) : string {
+		$query = unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+		return ($query && $query['status'] == 'success') ? $query['city'] : 'Unknown';
+	}
+
+}
